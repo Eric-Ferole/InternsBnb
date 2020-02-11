b5e136b7f0a07473cdd50efb944c2de7aaa9ce81
Éric Férole <eferole@pixelcircus.ca>
2020-02-11 08:18:13 -0500

    Adding infos in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+{ name:"Web developper", category:"IT", duration:"3-6 months" }
+name:"UX Designer"
+name:"UX/UI Designer"
+name:"Web designer"
+name:"Front End developer"
+name:"Back End developer"
+name:"Ruby Rails RoR Developer"
+name:"Full Stack PHP Developer"
+name:"Software Developer"
+name:"Software engineering"
+name:"Full Stack Developper"
+name:"Product owner"
+name:"Scrum master"
+name:"Project coordinator"
+
+business_name:"National Bank of Canada", street_address:"600 Rue de la Gauchetière O 28e étage", postal_code:"H3B 4L2", city:"Montreal", province:"Quebec", country:"Canada", website:"https://jobs.nbc.ca/"
+business_name:"Ivado Lab", street_address:"1100 Avenue des Canadiens-de-Montréal Suite 401", postal_code:"H3B 2S2", city:"Montreal", province:"Quebec", country:"Canada", website:"www.ivadolab.ca"
+business_name:"Lightspeed", street_address:"700 Rue Saint Antoine E", postal_code:"H2Y 1A6", city:"Montreal", province:"Quebec", country:"Canada", website:"https://www.lightspeedhq.com/careers/"
+business_name:"CGI", street_address:"1350 Boulevard René-Lévesque O", postal_code:"H3G 1T4", city:"Montreal", province:"Quebec", country:"Canada", website:"https://www.cgi.com/en/careers"
+business_name:"Google", street_address:"1253 Avenue McGill College", postal_code:"H3B 2Y5", city:"Montreal", province:"Quebec", country:"Canada", website:"https://careers.google.com/jobs/"
+business_name:"Lg2", street_address:"3575 Boul St-Laurent", postal_code:"H2X 2T7", city:"Montreal", province:"Quebec", country:"Canada", website:"www.lg2.com"
+business_name:"Pixel Circus", street_address:"204-3875 Rue Saint-Urbain", postal_code:"H2W 1V1", city:"Montreal", province:"Quebec", country:"Canada", website:"www.pixelcircus.ca"
+business_name:"Ubisoft", street_address:"5505 Boul St-Laurent #2000", postal_code:"H2T 1S6", city:"Montreal", province:"Quebec", country:"Canada", website:"https://montreal.ubisoft.com/en/your-career/"
+business_name:"AutoDesk", street_address:"10 Rue Duke", postal_code:"H3C 2L7", city:"Montreal", province:"Quebec", country:"Canada", website:"https://www.autodesk.com/careers"
+business_name:"Cirque du Soleil", street_address:"8400 2e Avenue", postal_code:"H1Z 4M6", city:"Montreal", province:"Quebec", country:"Canada", website:"https://www.cirquedusoleil.com/careers"
+business_name:"MindGeek", street_address:"7777 Boulevard Décarie", postal_code:"H4P 2H2", city:"Montreal", province:"Quebec", country:"Canada", website:"https://www.mindgeek.com/careers/"
+business_name:"Deloitte", street_address:"1190 Avenue des Canadiens-de-Montréal #500", postal_code:"H3B 0M7", city:"Montreal", province:"Quebec", country:"Canada", website:"https://careers.deloitte.ca/"
+business_name:"Warner Brothers Games Montréal", street_address:"888 Boulevard de Maisonneuve East", postal_code:"H2L 4M8", city:"Montreal", province:"Quebec", country:"Canada", website:"https://wbgamesmontreal.com/en/jobs"
+business_name:"CBC/Radio-Canada", street_address:"1400 Boulevard René-Lévesque E", postal_code:"H2L 2M2", city:"Montreal", province:"Quebec", country:"Canada", website:"https://cbc.radio-canada.ca/en/working-with-us/jobs"
+business_name:"Morgan Stanley", street_address:"700 Rue Wellington", postal_code:"H3C 3S4", city:"Montreal", province:"Quebec", country:"Canada", website:"https://www.morganstanley.com/people/people-montreal"
+business_name:"Morneau Shepell", street_address:"800 Rue du Square-Victoria", postal_code:"H4Z 0A4", city:"Montreal", province:"Quebec", country:"Canada", website:"https://www.morneaushepell.com/ca-en/careers"
+business_name:"CAE", street_address:"Chemin de la Côte-de-Liesse", postal_code:"H4T 1G6", city:"St-Laurent", province:"Quebec", country:"Canada", website:"https://www.cae.com/careers/"
+business_name:"Frank and Oak", street_address:"160 Rue St Viateur East", postal_code:"H2T 1A8", city:"Montreal", province:"Quebec", country:"Canada", website:"www.frankandoak.com"
