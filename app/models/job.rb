346a166312acb289e7b4c54a9c345d9e0fba50bd
Guillaume Lenzinger <glenzinger@gmail.com>
2020-02-11 20:56:55 -0500

    validations en cours

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,9 @@
 class Job < ApplicationRecord
   has_many :users, through: :requests, dependent: :destroy
   has_many :requests, dependent: :destroy
+
+  validates :description, length: { maximum: 500 }
+  validates :category, inclusion: { in: %w(Full-stack Developer, Web designer, Front-end developer, Back-end developer, UX/UI) }
+  validates :duration, inclusion: { in: %w(Full-stack Developer, Web designer, Front-end developer, Back-end developer, UX/UI) }
+  validates :name, presence: true
 end
