9c0ee82c0d4020610d4b479bcb4b8d7cc6d641ab
Éric Férole <eferole@pixelcircus.ca>
2020-02-13 19:04:22 -0500

    User view with image uploader working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
 
-  before_action :authenticate_user!
-  end
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  protected
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :company_name,
+    :street_address, :city, :province, :postal_code, :country, :website, :business_size, :logo])
+  end
+end
