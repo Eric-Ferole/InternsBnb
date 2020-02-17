7e7d6d9b13e3767dd075709745871e483afe9ceb
kirill ginko <kirillginko@gmail.com>
2020-02-17 14:56:25 -0500

    created request policy w pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ class ApplicationController < ActionController::Base
   # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   # def user_not_authorized
-  #   flash[:alert] = "You are not authorized to perform this action."
-  #   redirect_to(root_path)
+  # flash[:alert] = "You are not authorized to perform this action."
+  # redirect_to(root_path)
   # end
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :company_name,
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,36 @@
 class RequestsController < ApplicationController
+
+
+  def new
+    @request = Request.new
+    @job = Job.find(params[:job_id])
+    authorize @request
+  end
+
+  def create
+    @request = Request.new(request_params)
+    @request.user = current_user
+    authorize @request
+    @request.status = "pending"
+    if @request.save!
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    request = Request.find(params[:id])
+    authorize request
+    if request.update!(request_params)
+    redirect_to jobs_path
+  end
+  end
+
+
+  private
+
+  def request_params
+    params.require(:request).permit(:status, :user_id, :job_id, :id)
+  end
 end
