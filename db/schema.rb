0d4324e6f2fddecb54a657e6e58e2d2e005d21cc
Éric Férole <eferole@pixelcircus.ca>
2020-02-20 23:26:09 -0500

    quick fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_234805) do
+ActiveRecord::Schema.define(version: 2020_02_20_232814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,9 +64,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_234805) do
     t.string "phone_number"
     t.string "company_name"
     t.string "street_address"
-    t.string "city"
     t.string "province"
-    t.string "postal_code"
     t.string "country"
     t.string "website"
     t.string "logo"
