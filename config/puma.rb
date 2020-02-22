8ff1120111606a3ee0b1dcc72accbe4ce8d00096
Éric Férole <eferole@pixelcircus.ca>
2020-02-22 14:48:01 -0500

    Heroku

--- config/puma.rb
+++ config/puma.rb
@@ -26,6 +26,8 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
+# asdfl;kjasdf
+
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
